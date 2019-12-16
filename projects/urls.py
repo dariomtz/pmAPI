ff210c4a8c92bbc75eaa9f1384d57252ac2e5cd7
Dario Martinez <gusdamtzr@gmail.com>
2019-12-16 12:23:13 -0600

    Get for a specific project

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.projectHandler, name='projects'),
+    path('', views.all_projects, name='all-projects'),
+    path('<int:projectId>/', views.specific_project, name='specific-project')
 ]
--- projects/views.py
+++ projects/views.py
@@ -5,43 +5,72 @@ import json, datetime
 
 projects = {
     '1':{
-        'title': 'Dummy Project',
+        'kind': 'project',
         'id': '1',
+        'title': 'Dummy Project',
         'description': 'This is a project to test the feature that creates projects',
-        'user': 'Dario Martinez',
         'created': '2011-08-01T19:58:51.947',
         'updated': '2011-08-01T19:58:51.947',
-        'kind': 'project'
+        'canRead':[],
+        'canEdit':[],
+        'admins':[],
+        'tasks': {},
+        'deadline': '2020-08-01T19:58:51.947',
+        'status': 0,
     }
 }
 
 @csrf_exempt
-def projectHandler(request):
-    if request.method == 'GET':
-        response = { 'projects': projects }
-        return JsonResponse(response)
+def all_projects(request):
 
-    elif request.method == 'POST':
+    if request.method == 'GET':#get all projects
+        return JsonResponse({ 'projects': projects })
+
+    elif request.method == 'POST':#create a new project
         body = json.loads(request.body)
 
-        #validation
+        #validate that the request has everything that has been asked for
         if not 'title' in body:
             return HttpResponseBadRequest('A project cannot be created without title')
         if not 'description' in body:
             return HttpResponseBadRequest('A project cannot be created without description')
-
+        if not 'deadline' in body:
+            return HttpResponseBadRequest('A project cannot be created without a deadline')
+        
         #creation of new object
         id = str(len(projects) + 1)
         newProject = {
-            'title': body['title'],
-            'description': body['description'],
-            'user': 'Default user',
+            'kind': 'project',
             'id': id,
+            'title': body['title'], 
+            'description': body['description'],
             'created': datetime.datetime.now(),
             'updated': datetime.datetime.now(),
-            'kind': 'project'
+            'canRead':[],
+            'canEdit':[],
+            'admins':[],
+            'tasks': {},
+            'deadline': body['deadline'],
+            'status': 0,
         }
         projects[id] = newProject
         return JsonResponse(newProject)
 
+@csrf_exempt
+def specific_project(request, projectId=None):
+
+    if request.method == 'GET':#gets the specific project
+
+        if not str(projectId) in projects:
+            return HttpResponseBadRequest('A project with that id does not exist.')
+            
+        return JsonResponse(projects[str(projectId)])
         
+    elif request.method == 'POST':#post a task to the project
+        return
+    elif request.method == 'PUT':#modify the entire project to these new values
+        return
+    elif request.method == 'PATCH':#modify only the changes to the resource
+        return
+    elif request.method == 'DELETE':#deletes the project
+        return
