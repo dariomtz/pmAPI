ec90ce69e472ff2957e60656c6fd9d97ea4c26dd
Dario Martinez <gusdamtzr@gmail.com>
2020-01-09 13:16:54 -0600

    added assertions for valid and invalit tasks

--- projects/tests/testing_library.py
+++ projects/tests/testing_library.py
@@ -121,3 +121,21 @@ class AssertHelper(TestCase):
 
         project = response.json()
         self.assert_valid_project(project)  
+
+    def assert_post_valid_task(self, new_task):
+        response = self.client.post('/api/projects/' + self.valid_project_id() + '/', data=new_task, content_type='application/json')
+        
+        self.assertEquals(response.status_code, 200)
+
+        task = response.json()
+
+        self.assert_valid_task(task)
+
+    def assert_post_invalid_task(self, new_task):
+        response = self.client.post('/api/projects/' + self.valid_project_id() + '/', data=new_task, content_type='application/json')
+        
+        self.assertEquals(response.status_code, 400)
+
+        error = response.json()
+
+        self.assert_valid_task(error)
