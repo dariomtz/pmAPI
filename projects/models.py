f4dc902a1e128d43bb90d8b68cef396febc1e1e9
Dario Martinez <gusdamtzr@gmail.com>
2020-01-20 07:10:18 -0600

    project and task model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
-# Create your models here.
+class Project(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, blank=True)
+    description = models.CharField(blank=True)
+    can_read = models.ForeignKey(Group, on_delete=models.SET_NULL)
+    can_edit = models.ForeignKey(Group, on_delete=models.SET_NULL)
+    admins = models.ForeignKey(Group, on_delete=models.SET_NULL)
+    tasks = models.ManyToManyField('Task')
+    deadline = models.DateTimeField()
+    status = models.BooleanField(default=False)
+
+class Task(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, blank=True)
+    description = models.CharField(blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    deadline = models.DateTimeField()
+    startDate = models.DateTimeField()
+    inCharge = models.ManyToManyField(Group, on_delete=models.SET_NULL)
+    resources = models.CharField(blank=True)
+
