e6885465b2a5578c36da9075a600ffa0f38d1ba4
Dario Martinez <gusdamtzr@gmail.com>
2020-01-30 07:22:01 -0600

    Created new users django app

--- pmAPI/settings.py
+++ pmAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'api.apps.ApiConfig',
     'projects.apps.ProjectsConfig',
     'web.apps.WebConfig',
