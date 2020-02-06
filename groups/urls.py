3a007512128238f3e117b1157a1d747195c981be
Dario Martinez <gusdamtzr@gmail.com>
2020-02-06 08:44:45 -0600

    Basic paths and views

--- groups/urls.py
+++ groups/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.groups_view, name='projects'),
+    path('', views.groups_view, name='groups'),
+    path('<int:groupId>/', views.single_group_view, name='groups'),
+    path('<int:groupId>/projects/', views.add_project_group, name='groups'),
 ]
--- groups/views.py
+++ groups/views.py
@@ -9,3 +9,9 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def groups_view(request):
     return JsonResponse({})
+
+def single_group_view(request, groupId=None):
+    return JsonResponse({})
+
+def add_project_group(request,groupId=None):
+    return JsonResponse()
