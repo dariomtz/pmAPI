c8bb266194acc245d6d07502fe799b16126e25e3
Dario Martinez <gusdamtzr@gmail.com>
2020-02-06 08:19:31 -0600

    Added groups app

--- pmAPI/settings.py
+++ pmAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'groups.apps.GroupsConfig',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
     'projects.apps.ProjectsConfig',
