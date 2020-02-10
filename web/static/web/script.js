84800e4c1954f63acb97c008736780734546b723
Gustavo Sotres <gustavonline.games@gmail.com>
2020-02-10 14:48:49 -0600

    Fix Positon of boxes

--- web/static/web/script.js
+++ web/static/web/script.js
@@ -1,20 +1,3 @@
-function showPassword() {
-  var x = document.getElementById("password");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-} 
-function showR_Password() {
-  var x = document.getElementById("r_password");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-} 
-
 function toggleForm(){
   let register = document.getElementById("registro");
   let login = document.getElementById("login");
