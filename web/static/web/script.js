a35415c2d48d8a88d06e00ef07efd37a90633c52
Gustavo Sotres <gustavonline.games@gmail.com>
2020-02-11 14:06:02 -0600

    gettingData from forms
    Adding simple functions to get the info of the login and sigup forms

--- web/static/web/script.js
+++ web/static/web/script.js
@@ -1,6 +1,47 @@
-function toggleForm(){
+function toggleForm() {
   let register = document.getElementById("registro");
   let login = document.getElementById("login");
   register.classList.toggle("unseen");
   login.classList.toggle("unseen");
+}
+
+function getLoginData() {
+  let username = $("#l_username").val();
+  let password = $("#l_password").val();
+  let test = {
+    username: username,
+    password: password
+  };
+  console.log(test);
+  console.log(JSON.stringify(test));
+  return test;
+}
+
+function getSigUpData() {
+  let username = $("#username").val();
+  let name = $("#name").val();
+  let lastname = $("#last_name").val();
+  let email = $("#email").val();
+  let password = $("#password").val();
+  let sigUp = {
+    username: username,
+    name: name,
+    last_name: lastname,
+    email: email,
+    password: password
+  };
+  console.log(sigUp);
+  return sigUp;
+}
+
+function passwordMatch() {
+  return $("#password").val() === $("#r_password").val() ? true : false;
+}
+
+function sendSigUp() {
+  if (passwordMatch()) {
+    alert("coinciden");
+  } else {
+    alert("no coinciden");
+  }
 }
